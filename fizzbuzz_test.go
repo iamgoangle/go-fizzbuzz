846e663fb8a81f8f75f0bf17e1d9033cee57663d
Teerapong Singthong <teerapong.s@linecorp.com>
2019-01-05 11:21:21 +0700

    test: test fizzbuzz simple logic

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -1,7 +1,14 @@
 package fizzbuzz
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFizzBuzzShouldSayOne(t *testing.T) {
+	got := Say(1)
+	want := "1"
 
+	if got != want {
+		t.Errorf("it should say %q but got %q", got, want)
+	}
 }
