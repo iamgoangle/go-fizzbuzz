50bb37f223a8b1a6d8606b3fa1cf82fe24d6be35
Teerapong Singthong <teerapong.s@linecorp.com>
2019-01-05 11:24:55 +0700

    test: add test spec for fizzbuzz logic "2"

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,5 +1,9 @@
 package fizzbuzz
 
 func Say(n int) string {
+	if n == 2 {
+		return "2"
+	}
+
 	return "1"
 }
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -12,3 +12,12 @@ func TestFizzBuzzShouldSayOne(t *testing.T) {
 		t.Errorf("it should say %q but got %q", got, want)
 	}
 }
+
+func TestFizzBuzzShouldSayTwo(t *testing.T) {
+	got := Say(2)
+	want := "2"
+
+	if got != want {
+		t.Errorf("it should say %q but got %q", got, want)
+	}
+}
